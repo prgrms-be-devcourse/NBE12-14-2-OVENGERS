e5221f6e02d350590aebbac1432ee2aed76d7445
jkidse14 <jkidse14@gamil.com>
2026-09-14 14:05:36 +0900

    feat(payment): add payment response

--- backend/src/main/java/com/ovengers/slotkey/payment/dto/response/PaymentResponse.java
+++ backend/src/main/java/com/ovengers/slotkey/payment/dto/response/PaymentResponse.java
@@ -1,4 +1,22 @@
 package com.ovengers.slotkey.payment.dto.response;
 
-public class PaymentResponse {
+import com.ovengers.slotkey.payment.entity.Payment;
+import com.ovengers.slotkey.payment.entity.PaymentStatus;
+
+import java.time.LocalDateTime;
+
+public record PaymentResponse(
+        PaymentStatus status,
+        LocalDateTime paidAt,
+        LocalDateTime cancelledAt
+) {
+
+    // DTO 변환 메서드
+    public static PaymentResponse from(Payment payment){
+        return new PaymentResponse(
+                payment.getStatus(),
+                payment.getPaidAt(),
+                payment.getCancelledAt()
+        );
+    }
 }
