ea92b6219612c16d312f4e9d4d83f540f16c6b36
jkidse14 <jkidse14@gamil.com>
2026-09-14 15:43:32 +0900

    feat(payment): add payment table migration

--- backend/src/main/resources/db/migration/V4__create_payment_tables.sql
+++ backend/src/main/resources/db/migration/V4__create_payment_tables.sql
@@ -1,1 +1,12 @@
-백한비님
+CREATE TABLE payment  (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    reservation_id BIGINT NOT NULL ,
+    amount BIGINT NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    paid_at DATETIME(6) NOT NULL,
+    cancelled_at DATETIME(6),
+
+    PRIMARY KEY (id),
+    UNIQUE (reservation_id),
+    FOREIGN KEY (reservation_id) REFERENCES reservation(id)
+);
