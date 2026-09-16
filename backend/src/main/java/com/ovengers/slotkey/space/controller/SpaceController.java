00dc27c10a9c04b85cf7efb04e6b085d271c806c
Lemnoideae <aquan5584j@gmail.com>
2026-09-16 09:54:04 +0900

    feat: update API versioning in controllers to v1

--- backend/src/main/java/com/ovengers/slotkey/space/controller/SpaceController.java
+++ backend/src/main/java/com/ovengers/slotkey/space/controller/SpaceController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class SpaceController {
 
