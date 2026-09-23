22a157b5ed8b3a75e6de056e040631611546f13f
vvipia <vvipia@naver.com>
2026-09-23 09:35:32 +0900

    docs(swagger): API 문서 제목 및 설명과 버전 설정

--- backend/src/main/java/com/ovengers/slotkey/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/ovengers/slotkey/global/config/SwaggerConfig.java
@@ -1,4 +1,19 @@
 package com.ovengers.slotkey.global.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI slotKeyOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SlotKey API")
+                        .description("공간 예약 및 크레딧 결제 서비스 API 문서")
+                        .version("v1"));
+    }
 }
